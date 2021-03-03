22937bdab3fcbb5f9cf1d6bd4308c26597ccc4e0
Mr.MG <mg17.mohammad@gmail.com>
2021-03-04 00:52:00 +0330

    Material Style Added To App

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,5 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQuickStyle>
 
 int main(int argc, char *argv[])
 {
@@ -9,13 +10,31 @@ int main(int argc, char *argv[])
 
     QGuiApplication app(argc, argv);
 
+    app.setOrganizationName  ("MrMG");
+    app.setOrganizationDomain("MrMG");
+
+    app.setApplicationName("DoDiet");
+    app.setDesktopFileName("Do Diet");
+    app.setApplicationDisplayName("Do Diet");
+
+    app.setApplicationVersion(VERSION); // VERSION environment From .pro
+
     QQmlApplicationEngine engine;
+
+    //**************** App Style ****************//
+
+    QQuickStyle::setStyle("Material");
+
+    //*******************************************//
+
+    engine.addImportPath("qrc:/");
     const QUrl url(QStringLiteral("qrc:/main.qml"));
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
                      &app, [url](QObject *obj, const QUrl &objUrl) {
         if (!obj && url == objUrl)
             QCoreApplication::exit(-1);
     }, Qt::QueuedConnection);
+
     engine.load(url);
 
     return app.exec();
